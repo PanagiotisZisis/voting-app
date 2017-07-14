8e66c4692bbddc372bae4b2d0ccc6338fbe0297b
Panagiotis Zisis <takis.zisis@gmail.com>
2017-07-14 14:22:39 +0300

    added access control to dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,21 +5,25 @@ var router = express.Router();
 var User = require('../models/users');
 var Poll = require('../models/polls');
 
-router.get('/:username', function(req, res) {
-  if (!req.user) {
-    return res.redirect('/');
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+router.get('/:username', isLoggedIn, function(req, res) {
+  if (req.params.username !== req.user.username) {
+    return res.redirect('/dashboard/' + req.user.username);
   }
   res.render('dashboard', { user: req.user });
 });
 
-router.get('/', function(req, res) {
-  if (req.user) {
-    return res.redirect('/dashboard/ + ' + req.user.username);
-  }
-  res.redirect('/');
+router.get('/', isLoggedIn, function(req, res) {
+  res.redirect('/dashboard/' + req.user.username);
 });
 
-router.post('/:username/create', function(req, res) {
+router.post('/:username/create', isLoggedIn, function(req, res) {
   console.log(req.body);
   res.json(req.body);
 });
